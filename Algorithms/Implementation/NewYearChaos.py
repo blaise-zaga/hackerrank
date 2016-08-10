1e0300f8d65e2be503bcb3e9294f76b339646ee5
blaise-zaga <blaise.zaga@gmail.com>
2016-08-10 08:47:07 +0100

    An attempt to make New Year Chaos problem more readable.

--- Algorithms/Implementation/NewYearChaos.py
+++ Algorithms/Implementation/NewYearChaos.py
@@ -7,30 +7,36 @@ for a0 in xrange(T):
     n = int(raw_input().strip())
     q = map(int,raw_input().strip().split(' '))
     # your code goes here
-    count = 0
+    numberOfBribes = 0
+    numberOfBribesAnIndividualCanAfford = 2 #stated in question
     for i in xrange(len(q) - 1, 0, -1):
-        if count == -1:
+		# immediately you discover that the current state requires that
+		# people be more corrupt than we originally assumed, we stop the process
+        if numberOfBribes == -1:
             break
+
+        # in as much as the the referenced individual was not bribed,
+        # we continue searching for anybody that was bribed
         if q[i] > q[i-1]:
             continue
         else:
-            for j in xrange(i-1, i+1):
-                if j + 1 >= len(q) :
-                    break
-                if q[j] > q[j+1]:
-                    tmp = q[j]
-                    q[j] = q[j+1]
-                    q[j+1] = tmp
-                    count += 1
-            for j in xrange(i, i+2):
-                if j + 1 >= len(q):
-                    break
-                if q[j] > q[j+1]:
-                    count = -1
+			indexOfCorruptIndividual = i-1
+			for j in xrange(i - 1, i + 1):
+				if j + 1 >= len(q):
+					break
+				if q[j] > q[j+1]:
+					tmp = q[j]
+					q[j] = q[j+1]
+					q[j+1] = tmp
+					numberOfBribes += 1
+					indexOfCorruptIndividual = j + 1
 
-    if count == -1 :
+			if indexOfCorruptIndividual + 1 >= len(q):
+				continue
+			elif q[indexOfCorruptIndividual] > q[indexOfCorruptIndividual + 1]:
+				numberOfBribes = -1
+
+    if numberOfBribes == -1 :
         print "Too chaotic"
     else:
-        print count
-
-
+        print numberOfBribes
